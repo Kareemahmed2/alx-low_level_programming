ed817cd0ef2f7c3f74804de0329fb923247dad8b
Kareemahmed2 <ka.mahrous@gmail.com>
2023-02-17 05:48:28 -0800

    c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    for(char i=97;i<=122;++i)
+    {
+        putchar(i);
+    }
+    for(char i=65;i<=90;++i)
+    {
+        putchar(i);
+    }
+}
