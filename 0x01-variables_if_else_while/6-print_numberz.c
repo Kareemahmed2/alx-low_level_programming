f9333a88e04e71e383ec4d848effe29420b0a3e7
Kareemahmed2 <ka.mahrous@gmail.com>
2023-02-17 06:43:24 -0800

    c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char j;
+     for(int n=0;n<=9;n++)
+    {
+        
+        putchar('0'+n);
+    }
+}
