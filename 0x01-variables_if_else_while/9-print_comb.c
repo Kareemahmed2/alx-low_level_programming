3b6940ca3341f4e2150640986ccc330e821c6169
Kareemahmed2 <ka.mahrous@gmail.com>
2023-02-25 13:48:08 -0800

    c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-     for(int n=0;n<=9;n++)
-    {
-        
-        putchar('0'+n);
-        if(n!=9){
-             putchar(',');
-             putchar(' ');
-    }
-    }
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+		putchar(d);
+		if (d != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
 }
