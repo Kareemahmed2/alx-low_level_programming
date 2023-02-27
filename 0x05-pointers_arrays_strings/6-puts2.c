7ae110e69f464606bf195db6ce58b95fd46541ae
account name <your email>
2023-02-27 05:53:04 -0800

    c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,10 +10,10 @@ void puts2(char *str)
 	int len = 0, i = 0;
 
 	while (str[len] != '\0')
-	       	len++;
+		len++;
 
 	for (; i < len; i += 2)
-	       _putchar(str[i]);
+		_putchar(str[i]);
 
 	_putchar('\n');
 }
