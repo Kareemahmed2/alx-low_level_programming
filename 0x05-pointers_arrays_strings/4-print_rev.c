c1cfefc6123cb2d8ef089b15fc80e51bf67c5641
account name <your email>
2023-02-27 04:12:22 -0800

    c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,19 +7,14 @@
 void print_rev(char *s)
 {
 	int longi = 0;
-	int o;
 
-	while (*s != '\0')
+	while (s[longi] != '\0')
 	{
 		longi++;
-		s++;
 	}
-	s--;
-	for (o = longi; o > 0; o--)
-	{
-		_putchar(*s);
-		s--;
-	}
+	while(len){
+		_putchar(s[--longi]);
+		}
 
 	_putchar('\n');
 }
