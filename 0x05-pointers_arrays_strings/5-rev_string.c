dac2dc72556c45ab29378318b36b8f165c891ad3
account name <your email>
2023-02-27 05:28:47 -0800

    c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,7 +1,8 @@
 #include"main.h"
 
-/*
- * rev_string - prints the string reversed
+/**
+ * rev_string - a function that reverse a string
+ *  followed by a new line.
  * @s: input string
  * Return: string in reverse
  */
