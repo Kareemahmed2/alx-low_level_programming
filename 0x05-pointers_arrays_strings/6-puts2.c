281ab249787b5b84c0c434993907455e4880d7d7
account name <your email>
2023-02-27 05:50:16 -0800

    c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ * puts2 -a function that prints every other character of a string
+ * @str: input string
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+	int len = 0, i = 0;
+
+	while (str[len] != '\0')
+	       	len++;
+
+	for (; i < len; i += 2)
+	       _putchar(str[i]);
+
+	_putchar('\n');
+}
